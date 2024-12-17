8b6af82e9e22d349b311b93165736f0a4731926e
= <=>
2024-12-17 11:48:36 +0300

    prod url

--- react/api/axios.ts
+++ react/api/axios.ts
@@ -42,8 +42,8 @@ const getDefaultConfig = (config: InternalAxiosRequestConfig<any>) => {
 
 // Create an Axios instance
 const baseApi = axios.create({
-    // baseURL: "https://api.spacedesk.sa/api/v1",
-    baseURL: "https://spacedesk.digital-pages.work/api/v1",
+    baseURL: "https://api.spacedesk.sa/api/v1",
+    // baseURL: "https://spacedesk.digital-pages.work/api/v1",
     timeout: 10000,
 });
 
